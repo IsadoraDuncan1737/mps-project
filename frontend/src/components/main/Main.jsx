db7957e8a29301af0ed00bb6369533ed1cc8367a
maria ryabikovskaya <ryabikovskay@gmail.com>
2023-12-02 01:34:19 +0300

    feat: add main page

---
+++ frontend/src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+
+const Main = () => {
+  return (
+    <Box>
+      <Typography>yep main page</Typography>
+    </Box>
+  );
+};
+
+export default Main;
