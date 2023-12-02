83be409138879a1d38796328bc66256798296fe0
maria ryabikovskaya <ryabikovskay@gmail.com>
2023-12-03 01:19:55 +0300

    feat: add temporary data files

---
+++ frontend/src/components/main/data/users.js
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    ID: '101',
+    Login: 'valera',
+  },
+  {
+    ID: '123',
+    Login: 'ne valera',
+  },
+  {
+    ID: '789',
+    Login: 'tochno ne valera',
+  },
+];
