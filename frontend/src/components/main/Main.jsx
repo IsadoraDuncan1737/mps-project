2d60a835756b0426f3b31b0f27b70e45201074d0
maria ryabikovskaya <ryabikovskay@gmail.com>
2023-12-02 16:52:36 +0300

    feat: add sight card to main

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -1,9 +1,30 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
+import styles from './Main.module.css';
+import { sights } from './data/sights';
+import SightCard from './sight-card/SightCard';
 
 const Main = () => {
   return (
-    <Box>
-      <Typography>yep main page</Typography>
+    <Box className={styles.wrapper}>
+      <Typography className={styles.page_title}>
+        Исследуйте и создавайте достопримечательности вместе с другими
+        пользователями!
+      </Typography>
+      <Grid container spacing={5}>
+        {sights.map((item) => (
+          <SightCard
+            key={item.ID}
+            id={item.ID}
+            title={item.Title}
+            location={item.Location}
+            image={item.Image}
+            summary={item.Summary}
+            date={item.FoundingDate}
+            type={item.Type}
+            userId={item.UserID}
+          />
+        ))}
+      </Grid>
     </Box>
   );
 };
