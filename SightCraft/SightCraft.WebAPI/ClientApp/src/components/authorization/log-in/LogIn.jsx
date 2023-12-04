1799093b720093f953b10fdfc70a3dba47fd6bdc
maria ryabikovskaya <ryabikovskay@gmail.com>
2023-12-04 23:29:12 +0300

    fix: fix rerender bug

--- SightCraft/SightCraft.WebAPI/ClientApp/src/components/authorization/log-in/LogIn.jsx
+++ SightCraft/SightCraft.WebAPI/ClientApp/src/components/authorization/log-in/LogIn.jsx
@@ -1,11 +1,11 @@
 import {
-  Box,
-  Typography,
-  Divider,
-  Input,
-  FormControl,
-  FormHelperText,
-  Button,
+    Box,
+    Typography,
+    Divider,
+    Input,
+    FormControl,
+    FormHelperText,
+    Button,
 } from '@mui/material';
 import styles from '../Authorization.module.css';
 import { useForm } from 'react-hook-form';
@@ -15,73 +15,75 @@ import { NavPathes } from '../../../utils/navpathes';
 import { Endpoints } from '../../../utils/endpoints';
 
 const LogIn = () => {
-  let navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+    let navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
-  async function onSubmit(value) {
-    const userData = {
-      Login: value.login,
-      Password: value.password,
-    };
+    async function onSubmit(value) {
+        const userData = {
+            Login: value.login,
+            Password: value.password,
+        };
 
-    axios
-      .post(Endpoints.USER_LOGIN(), userData)
-      .then((res) => {
-        console.log(res);
-        sessionStorage.setItem('token', res.data.token);
-        navigate(NavPathes.MAIN_PAGE());
-      })
-      .catch((e) => <Typography>Ошибка: {e}</Typography>);
-  }
+        let response = await axios
+            .post(Endpoints.USER_LOGIN(), userData)
+            .then((res) => {
+                console.log(res);
+                sessionStorage.setItem('token', res.data.token);
+                if (res.status === 200) {
+                    navigate(NavPathes.MAIN());
+                }
+            })
+            .catch((e) => console.log(e));
+    }
 
-  return (
-    <Box className={styles.wrapper}>
-      <Box className={styles.form_box}>
-        <Box className={styles.title_wrapper}>
-          <Typography className={styles.title}>Вход</Typography>
-          <Divider className={styles.line} />
+    return (
+        <Box className={styles.wrapper}>
+            <Box className={styles.form_box}>
+                <Box className={styles.title_wrapper}>
+                    <Typography className={styles.title}>Вход</Typography>
+                    <Divider className={styles.line} />
+                </Box>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FormControl className={styles.form_control}>
+                        <Input
+                            placeholder="Введите логин"
+                            type="text"
+                            autoFocus={true}
+                            className={styles.input}
+                            inputProps={{ maxLength: 40, minLength: 3 }}
+                            {...register('login', { required: true })}
+                        ></Input>
+                        <FormHelperText error={!!errors.login}>
+                            {errors.login?.message}
+                        </FormHelperText>
+                    </FormControl>
+                    <FormControl className={styles.form_control}>
+                        <Input
+                            placeholder="Введите пароль"
+                            type="password"
+                            className={styles.input}
+                            inputProps={{ maxLength: 20, minLength: 5 }}
+                            {...register('password', {
+                                required: true,
+                            })}
+                        ></Input>
+                        <FormHelperText error={!!errors.password}>
+                            {errors.password?.message}
+                        </FormHelperText>
+                    </FormControl>
+                    <Box className={styles.button_wrapper}>
+                        <Button variant="outlined" type="submit">
+                            Войти
+                        </Button>
+                    </Box>
+                </form>
+            </Box>
         </Box>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormControl className={styles.form_control}>
-            <Input
-              placeholder="Введите логин"
-              type="text"
-              autoFocus={true}
-              className={styles.input}
-              inputProps={{ maxLength: 40, minLength: 3 }}
-              {...register('login', { required: true })}
-            ></Input>
-            <FormHelperText error={!!errors.login}>
-              {errors.login?.message}
-            </FormHelperText>
-          </FormControl>
-          <FormControl className={styles.form_control}>
-            <Input
-              placeholder="Введите пароль"
-              type="password"
-              className={styles.input}
-              inputProps={{ maxLength: 20, minLength: 5 }}
-              {...register('password', {
-                required: true,
-              })}
-            ></Input>
-            <FormHelperText error={!!errors.password}>
-              {errors.password?.message}
-            </FormHelperText>
-          </FormControl>
-          <Box className={styles.button_wrapper}>
-            <Button variant="outlined" type="submit">
-              Войти
-            </Button>
-          </Box>
-        </form>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default LogIn;
--- SightCraft/SightCraft.WebAPI/ClientApp/src/components/common/header/Header.jsx
+++ SightCraft/SightCraft.WebAPI/ClientApp/src/components/common/header/Header.jsx
@@ -1,53 +1,62 @@
 import { AppBar, Box, Divider, Grid, Typography } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 import checkTokenExpiration from '../../../check-token-expiration/checkTokenExpiration';
 import { useEffect, useState } from 'react';
 import { NavPathes } from '../../../utils/navpathes';
 
 const Header = () => {
-  const [isTokenAlive, setIsTokenAlive] = useState(false);
-  useEffect(() => {
-    setIsTokenAlive(checkTokenExpiration());
-  }, [isTokenAlive]);
-  
-  return (
-    <header>
-      <AppBar className={styles.appbar}>
-        <Grid container className={styles.container}>
-          <Grid item xs={3}>
-            <Typography className={styles.title}>SightCraft</Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <Box className={styles.links}>
-              <NavLink to={NavPathes.MAIN()}>Главная</NavLink>
-              {isTokenAlive ? (
-                <>
-                  <NavLink>Ваши достопримечательности</NavLink>
-                  <NavLink>Добавить еще!</NavLink>
-                </>
-              ) : (
-                false
-              )}
-            </Box>
-          </Grid>
-          <Grid item xs={3}>
-            <Box className={styles.auth_links}>
-              {isTokenAlive ? (
-                <NavLink>Выход</NavLink>
-              ) : (
-                <>
-                  <NavLink to={NavPathes.LOG_IN()}>Вход</NavLink>
-                  <NavLink to={NavPathes.REGISTRATION()}>Регистрация</NavLink>
-                </>
-              )}
-            </Box>
-          </Grid>
-        </Grid>
-        <Divider className={styles.line} />
-      </AppBar>
-    </header>
-  );
+    const [isTokenAlive, setIsTokenAlive] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setIsTokenAlive(()=>checkTokenExpiration());
+    }, [location.pathname, isTokenAlive]);
+    let navigate = useNavigate();
+
+    function onExitClick() {
+        sessionStorage.removeItem('token');
+        setIsTokenAlive(() => false);
+        navigate(NavPathes.MAIN());
+    }
+
+    return (
+        <header>
+            <AppBar className={styles.appbar}>
+                <Grid container className={styles.container}>
+                    <Grid item xs={3}>
+                        <Typography className={styles.title}>SightCraft</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Box className={styles.links}>
+                            <NavLink to={NavPathes.MAIN()}>Главная</NavLink>
+                            {isTokenAlive ? (
+                                <>
+                                    <NavLink>Ваши достопримечательности</NavLink>
+                                    <NavLink>Добавить еще!</NavLink>
+                                </>
+                            ) : (
+                                false
+                            )}
+                        </Box>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Box className={styles.auth_links}>
+                            {isTokenAlive ? (
+                                <NavLink onClick={onExitClick}>Выход</NavLink>
+                            ) : (
+                                <>
+                                    <NavLink to={NavPathes.LOG_IN()}>Вход</NavLink>
+                                    <NavLink to={NavPathes.REGISTRATION()}>Регистрация</NavLink>
+                                </>
+                            )}
+                        </Box>
+                    </Grid>
+                </Grid>
+                <Divider className={styles.line} />
+            </AppBar>
+        </header>
+    );
 };
 
 export default Header;
