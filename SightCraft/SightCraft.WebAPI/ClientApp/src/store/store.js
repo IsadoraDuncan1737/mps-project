bde987547c5a2e3cc3ab600edc7dc703eff930ca
maria ryabikovskaya <ryabikovskay@gmail.com>
2023-12-06 22:28:45 +0300

    feat: add slices to store

--- SightCraft/SightCraft.WebAPI/ClientApp/src/store/store.js
+++ SightCraft/SightCraft.WebAPI/ClientApp/src/store/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sightSlice from '../components/sight-detailed/sightSlice';
 import sightCreationSlice from '../components/sight-creation/sightCreationSlice';
+import mainSlice from '../components/main/mainSlice';
 
 export const store = configureStore({
-  reducer: { sight: sightSlice, sightCreation: sightCreationSlice },
+  reducer: {
+    sight: sightSlice,
+    sightCreation: sightCreationSlice,
+    main: mainSlice,
+  },
 });
