2f56932c1cae093e275f32e48e245d10fe3eb33b
maria ryabikovskaya <ryabikovskay@gmail.com>
2023-12-06 22:28:58 +0300

    feat: add new endpoints

--- SightCraft/SightCraft.WebAPI/ClientApp/src/utils/endpoints.js
+++ SightCraft/SightCraft.WebAPI/ClientApp/src/utils/endpoints.js
@@ -4,4 +4,10 @@ export const Endpoints = {
   USER_REGISTRATION: () => `${BASE_URL}/identity/registration`,
   USER_LOGIN: () => `${BASE_URL}/identity/login`,
   SIGHT_CREATE: () => `${BASE_URL}/sight`,
+  SIGHTS_GETALL: () => `${BASE_URL}/sight`,
+  SIGHT_GETBYID: (id) => `${BASE_URL}/sight/${id}`,
+  USER_GETBYID: (id) => `${BASE_URL}/user/${id}`,
+  USER_GETCURRENT: () => `${BASE_URL}/user/current-user`,
+  SIGHT_DELETE: (id) => `${BASE_URL}/sight/${id}`,
+  SIGHT_PUT: (id) => `${BASE_URL}/sight/${id}`,
 };
